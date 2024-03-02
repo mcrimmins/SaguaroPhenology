7bf2983535dcdb2a8dd544f1d8aae952a8d04745
mcrimmins <crimmins@email.arizona.edu>
2024-03-02 09:23:13 -0700

    more climate plots

--- analyzeClimate.R
+++ analyzeClimate.R
@@ -1,8 +1,15 @@
+#####
+# code to download daily PRISM climate data from single location
+# and process into cumulative precip and gdd
+# MAC 03/01/24
 
+# required libraries
 library(RCurl)
 library(jsonlite)
 library(dplyr)
-
+#library(lubridate)
+library(tidyr)
+library(ggplot2)
 
 #####
 # adjusted year function
@@ -56,21 +63,23 @@ dataDaily$month<-as.numeric(format(dataDaily$date, "%m"))
 dataDaily$year<-as.numeric(format(dataDaily$date, "%Y"))
 dataDaily$doy<-as.numeric(format(dataDaily$date, "%j"))
 
+save(dataDaily,lat,lon, file = "PRISM_dailyData.RData")
+######
+
+
+
+######### ANALYZE DATA -------
+##### load saved data
+load("PRISM_dailyData.RData")
+
 # create non-standard date like water year doy/year
-startMo<-10
+startMo<-7
 dataDaily$adjYear<-adj_yr(dataDaily$date, startMo)
 dataDaily$adjDOY<-adj_doy(dataDaily$date, startMo)
 
 # calc day avgT with base temp for gdd
-baseT<-50
+baseT<-10
 dataDaily$avgT_base <- unlist(lapply(dataDaily$avgT, function(x) ifelse(x>=baseT, x-baseT, 0)))
-
-# cumulative precip totals
-cumPrecip <- dataDaily %>% 
-  group_by(adjYear, adjDOY) %>% # still doesn't quite work adjYear kicks off before adjDOY
-  summarise(value = sum(precip, na.rm = T)) %>%
-  mutate(csum = cumsum(value))
-dataDaily$cumPrecip<-cumPrecip$csum
 
 # cumulative doy climate - precip and temp
 cumClim <- dataDaily %>% 
@@ -81,24 +90,194 @@ cumClim <- dataDaily %>%
          tsum = cumsum(temp))
 dataDaily$cumPrecip<-cumClim$psum
 dataDaily$gdd<-cumClim$tsum
+#####
 
 ##### analyze data ----
 
-library(ggplot2)
-
+#####
 # daily climatology
 dailyClimo<-dataDaily %>% group_by(adjDOY) %>%
   summarise(meanT=mean(avgT),
             meanP=mean(precip))
 
+# add in dummy date
+dailyClimo$date<-seq.Date(as.Date(paste0("2019-",startMo,"-01")),by="day",length.out = nrow(dailyClimo))
+
+# plot climograph
 ggplot(data = dailyClimo,
-       mapping = aes(x = adjDOY, y = meanT, group = 1)) + 
+       mapping = aes(x = date, y = meanT, group = 1)) + 
   geom_bar(mapping = aes(y = meanP*5), stat = "identity") + 
   geom_line() + 
   scale_y_continuous(
     "temperature, C", 
     sec.axis = sec_axis(~ . / 5, name = "precipitation, mm")
   ) +
+  scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 month",
+               date_labels = "%b")+
+  theme_bw()+
+  ggtitle(paste0("Daily Average Temperature and Precipitation (1981-2023, ",lat,", ",lon,")"))
+#####
+
+
+##### plot cumulative precip
+# reformat dataframe
+temp<- dataDaily[,c("adjDOY","adjYear","cumPrecip")] %>%  pivot_wider(names_from = "adjYear",values_from = "cumPrecip")
+temp <- temp[order(temp$adjDOY),]
+temp$date<-seq.Date(as.Date(paste0("2019-",startMo,"-01")),by="day",length.out = nrow(temp))
+
+temp<-temp %>% pivot_longer(cols=c(2:(ncol(temp)-1)),
+                    names_to='adjYear',
+                    values_to='precip')
+temp$adjYear<-as.numeric(temp$adjYear)
+
+# make plot
+trimYear<-2003
+ggplot(subset(temp,adjYear>trimYear), aes(date,precip, color=as.factor(adjYear)))+
+  geom_line()+
+  scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 month",
+               date_labels = "%b")+
+  theme_bw()+
+  ggtitle(paste0("Cumulative Daily Precipitation (mm) [",trimYear+1,"-2023, ",lat,", ",lon,"]"))
+#####
+
+##### plot gdd
+# reformat dataframe
+temp<- dataDaily[,c("adjDOY","adjYear","gdd")] %>%  pivot_wider(names_from = "adjYear",values_from = "gdd")
+temp <- temp[order(temp$adjDOY),]
+temp$date<-seq.Date(as.Date(paste0("2019-",startMo,"-01")),by="day",length.out = nrow(temp))
+
+temp<-temp %>% pivot_longer(cols=c(2:(ncol(temp)-1)),
+                            names_to='adjYear',
+                            values_to='gdd')
+temp$adjYear<-as.numeric(temp$adjYear)
+
+# plot gdd 
+ggplot(subset(temp,adjYear>2003), aes(date,gdd, color=as.factor(adjYear)))+
+  geom_line()+
+  scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 month",
+               date_labels = "%b")+
+  theme_bw()+
+  ggtitle(paste0("Growing degree days (C, baseT 10C) [",trimYear+1,"-2023, ",lat,", ",lon,"]"))
+#####
+
+##### plot daily data
+# make combined plot
+ggplot(data = dataDaily) + 
+  geom_bar(mapping = aes(x = date,y = precip/1), stat = "identity", color="forestgreen") + 
+  geom_line(aes(x = date, y = minT, group = 1), color="blue") +
+  geom_line(aes(x = date, y = maxT, group = 1), color="red") + 
+  scale_y_continuous(
+    "temperature, C", 
+    sec.axis = sec_axis(~ . *1, name = "precipitation, mm")
+  ) +
+  scale_x_date(date_breaks = "2 year", date_minor_breaks = "1 year",
+               date_labels = "%Y")+
+  theme_bw()+
+  ggtitle(paste0("Daily Average Temperature and Precipitation (1981-2023, ",lat,", ",lon,")"))
+#####
+#separate plots
+library(cowplot)
+p1<-ggplot(data = subset(dataDaily,year>2003)) + 
+  geom_line(aes(x = date, y = minT, group = 1), color="blue") +
+  geom_line(aes(x = date, y = maxT, group = 1), color="red") + 
+  scale_y_continuous(
+    "temperature, C") +
+  scale_x_date(date_breaks = "2 year", date_minor_breaks = "1 year",
+               date_labels = "%Y")+
+  theme_bw()+
+  ggtitle(paste0("Daily Average Temperature and Precipitation (1981-2023, ",lat,", ",lon,")"))
+p2<-ggplot(data = subset(dataDaily,year>2003)) + 
+  geom_bar(mapping = aes(x = date,y = precip/1), stat = "identity", color="forestgreen") + 
+  scale_y_continuous(
+    "precipitation, mm", 
+  ) +
+  scale_x_date(date_breaks = "2 year", date_minor_breaks = "1 year",
+               date_labels = "%Y")+
   theme_bw()
+plot_grid(p1,p2,labels="",ncol = 1)
+#####
+
+#####
+# annual precip and temps
+temp<- dataDaily %>% group_by(adjYear) %>%
+            summarise(precip=sum(precip, na.rm = TRUE),
+                      meanT=mean(avgT, na.rm = TRUE))
+
+p1<-ggplot(subset(temp, adjYear>2003 & adjYear<2024), aes(adjYear,precip))+
+  geom_bar(stat="identity")+
+  geom_hline(yintercept = mean(temp$precip))+
+  theme_bw()+
+  ggtitle(paste0("Annual Average Temperature and Precipitation (2004-2023, ",lat,", ",lon,")"))
+
+p2<-ggplot(subset(temp, adjYear>2003 & adjYear<2024), aes(adjYear,meanT))+
+  geom_bar(stat="identity")+
+  geom_hline(yintercept = mean(temp$meanT))+
+  coord_cartesian(ylim=c(15,22.5))+
+  theme_bw()
+
+plot_grid(p1,p2,labels="",ncol = 1)
+
+#####
+
+
+#####
+# precip anomalies
+temp<- dataDaily[,c("adjDOY","adjYear","cumPrecip")] %>%  pivot_wider(names_from = "adjYear",values_from = "cumPrecip")
+temp <- temp[order(temp$adjDOY),]
+
+temp<-temp %>% 
+  rowwise() %>% 
+  mutate(med = median(c_across(2:ncol(temp)), na.rm = TRUE))
+temp <- temp %>% mutate(across(2:(ncol(temp)-1), ~ .x - med))
+
+temp$date<-seq.Date(as.Date(paste0("2019-",startMo,"-01")),by="day",length.out = nrow(temp))
+
+temp<-temp %>% pivot_longer(cols=c(2:(ncol(temp)-2)),
+                            names_to='adjYear',
+                            values_to='precip')
+temp$adjYear<-as.numeric(temp$adjYear)
+
+# make plot
+trimYear<-2003
+p<-ggplot(subset(temp,adjYear>trimYear), aes(date,precip, color=as.factor(adjYear)))+
+  geom_line()+
+  scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 month",
+               date_labels = "%b")+
+  theme_bw()+
+  geom_hline(yintercept = 0)+
+  ggtitle(paste0("Cumulative Daily Precipitation Anomaly (mm) [",trimYear+1,"-2023, ",lat,", ",lon,"]"))
+plotly::ggplotly(p)
+#####
+
+#####
+# temp anomalies
+temp<- dataDaily[,c("adjDOY","adjYear","gdd")] %>%  pivot_wider(names_from = "adjYear",values_from = "gdd")
+temp <- temp[order(temp$adjDOY),]
+
+temp<-temp %>% 
+  rowwise() %>% 
+  mutate(med = median(c_across(2:ncol(temp)), na.rm = TRUE))
+temp <- temp %>% mutate(across(2:(ncol(temp)-1), ~ .x - med))
+
+temp$date<-seq.Date(as.Date(paste0("2019-",startMo,"-01")),by="day",length.out = nrow(temp))
+
+temp<-temp %>% pivot_longer(cols=c(2:(ncol(temp)-2)),
+                            names_to='adjYear',
+                            values_to='gdd')
+temp$adjYear<-as.numeric(temp$adjYear)
+
+# make plot
+trimYear<-2003
+p<-ggplot(subset(temp,adjYear>trimYear), aes(date,gdd, color=as.factor(adjYear)))+
+  geom_line()+
+  scale_x_date(date_breaks = "1 month", date_minor_breaks = "1 month",
+               date_labels = "%b")+
+  theme_bw()+
+  geom_hline(yintercept = 0)+
+  ggtitle(paste0("GDD anomaly (mm) [",trimYear+1,"-2023, ",lat,", ",lon,"]"))
+plotly::ggplotly(p)
+#####
+
+
 
 
